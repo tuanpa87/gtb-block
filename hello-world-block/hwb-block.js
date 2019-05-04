3b315992822005ce62873a138d9cec2ca470a72d
Tuanpa <tuan.pa.1987@gmail.com>
2019-05-04 18:42:39 +0700

    update

--- hello-world-block/hwb-block.js
+++ hello-world-block/hwb-block.js
@@ -1,16 +1,34 @@
-(function(blocks, element) {
+(function(blocks, element, editor) {
   var el = element.createElement;
   var registerBlockType = blocks.registerBlockType;
+  var PlainText = editor.PlainText;
+
   registerBlockType("hwb/block", {
     title: "hwb-block",
     category: "common",
+    attributes: {
+      text: {
+        source: "children",
+        selector: "p",
+        default: "default text"
+      }
+    },
 
     edit: function(props) {
-      return el("p", { className: "hwp-block" }, "hello");
+      //console.log("edit", props);
+      return el(PlainText, {
+        className: "hwp-block",
+        onChange: function(value) {
+          //console.log(value)
+          props.setAttributes({ text: value });
+        },
+        value: props.attributes.text
+      });
     },
 
     save: function(props) {
-      return el("p", { className: "hwp-block" }, "hello");
+      //console.log("save", props);
+      return el("p", { className: "hwp-block" }, props.attributes.text);
     }
   });
-})(window.wp.blocks, window.wp.element);
+})(window.wp.blocks, window.wp.element, window.wp.editor);
